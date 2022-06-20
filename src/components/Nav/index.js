c6c11d6498d8f5af18bceab990a3105af6c9e90c
Israel Alves <ysrael.2009@hotmail.com>
2022-06-19 22:15:30 -0300

    href changed

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,10 +6,10 @@ export default function index() {
     <div>
       <nav>
         <Menu>
-          <li><a href='javascript:;'>Find Property</a></li>
-          <li><a href='javascript:;'>Share Stories</a></li>
-          <li><a href='javascript:;'>Rental Guides</a></li>
-          <li><a href='javascript:;'>Download Mobile App</a></li>
+          <li><a href='./'>Find Property</a></li>
+          <li><a href='./'>Share Stories</a></li>
+          <li><a href='./'>Rental Guides</a></li>
+          <li><a href='./'>Download Mobile App</a></li>
         </Menu>
       </nav>
     </div>
